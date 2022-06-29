22987f0e3db8726036eee8525bc0d2b524378c3f
Yusuf-Karakaya <63005976+Yusuf-Karakaya@users.noreply.github.com>
2022-06-29 10:47:39 +0300

    mQTT_haberlesme

---
+++ mQTT_haberlesme/Consumer/Subscriber.cs
@@ -0,0 +1,38 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Text;
+
+namespace Subscriber
+{
+    class Subscriber
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                                .WithClientId(Guid.NewGuid().ToString())
+                                .WithTcpServer("test.mosquitto.org", 1883)
+                                .WithCleanSession()
+                                .Build();
+
+            if (client.IsConnected)
+            {
+                var topicFilter = new MqttTopicFilterBuilder()
+                                .WithTopic("yusuf")
+                                .Build();
+                await client.SubscribeAsync(topicFilter);
+            }
+
+                Console.WriteLine(MqttApplicationMessageExtensions.ConvertPayloadToString);
+
+            await client.ConnectAsync(options);
+
+           
+            Console.ReadLine();
+
+            await client.DisconnectAsync();
+        }
+    }
+}
---
+++ mQTT_haberlesme/mQTT_haberlesme/Publisher.cs
@@ -0,0 +1,47 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+
+
+namespace mQTT_haberlesme
+{
+    class Publisher
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            var options =new MqttClientOptionsBuilder()
+                                .WithClientId(Guid.NewGuid().ToString())
+                                .WithTcpServer("test.mosquitto.org",1883)
+                                .WithCleanSession()
+                                .Build();
+            await client.ConnectAsync(options);
+
+            Console.WriteLine("mesaj göndermek için bir tuşa basınız.");
+            Console.ReadLine();
+
+            await   PublishMessageAsync(client);
+
+            await client.DisconnectAsync();
+
+
+        }
+
+        private static async Task PublishMessageAsync(IMqttClient client)
+        {
+            string messagePayLoad = "Hello";
+            var message = new MqttApplicationMessageBuilder()
+                                    .WithTopic("yusuf")
+                                    .WithPayload(messagePayLoad)
+                                    .WithQualityOfServiceLevel(qualityOfServiceLevel:MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce)                
+                                    .Build();
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+            }
+        }
+        
+    }
+   
+}
