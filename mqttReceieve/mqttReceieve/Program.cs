8d8ffbbd3afbe859e6c4ddfec57e6d341bc3c544
Yusuf-Karakaya <63005976+Yusuf-Karakaya@users.noreply.github.com>
2022-06-29 10:49:34 +0300

    mqttReceieve

---
+++ mqttReceieve/mqttReceieve/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Net;
+namespace Mqtt_Haberlesme_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MqttClient mqttClientRecieve = new MqttClient("broker.hivemq.com");
+            mqttClientRecieve.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+            string clientId = Guid.NewGuid().ToString();
+
+
+            mqttClientRecieve.Connect(clientId);
+            Console.WriteLine("Abone olundu : test/deneme");
+            mqttClientRecieve.Subscribe(new string[] { "test/deneme" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            //birden fazla topic varsa hata veiyormuş
+           
+
+        }
+         static void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+         {
+            var message = System.Text.Encoding.Default.GetString(e.Message);
+            Console.WriteLine("alınan mesaj :" + message);
+         }
+
+    }
+}
+
+
